c11e7d5e66e3ac4c1daf0b08d4d3671d72309f26
Finn Chen <finn.chen.maenhaut@gmail.com>
2022-12-26 19:05:12 +0100

    commit tree 6cf1b1e51c47814f7ea78c06607ce4ee0583109f

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solutions in this file
+// const gifts = ["Teddy Bear", "drone", "doll"];
+
+// function wrapGifts(gifts) {
+//   for (let i = 0; i < gifts.length; i++) {
+//     console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//   }
+//   return gifts;
+// }
+// wrapGifts(gifts);
+
+function writeCards(stringNames, event) {
+  const names = [];
+  for (let i = 0; i < stringNames.length; i++) {
+    names.push(
+      `Thank you, ${stringNames[i]}, for the wonderful ${event} gift!`
+    );
+  }
+  return names;
+}
+
+function countDown() {
+  let count = 10;
+  while (count >= 0) {
+    console.log(count--);
+  }
+}
